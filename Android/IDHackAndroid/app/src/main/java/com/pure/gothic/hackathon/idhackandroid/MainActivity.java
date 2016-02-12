b0e8e005763e907a9bae6fde7f9af50f924a8e41
yohanchoi <xkth342@gmail.com>
2016-02-12 14:56:36 -0500

    <yohan> yo~

---
+++ Android/IDHackAndroid/app/src/main/java/com/pure/gothic/hackathon/idhackandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.pure.gothic.hackathon.idhackandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+// jae, lemonhall2, CYhan
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
