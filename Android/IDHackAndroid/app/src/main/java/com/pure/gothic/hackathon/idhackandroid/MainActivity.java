c75cfb83c997f19307edf67895f9cfc7acbabf18
darkstars77 <darkstars77@naver.com>
2016-02-13 05:32:44 +0900

    participate gon

--- Android/IDHackAndroid/app/src/main/java/com/pure/gothic/hackathon/idhackandroid/MainActivity.java
+++ Android/IDHackAndroid/app/src/main/java/com/pure/gothic/hackathon/idhackandroid/MainActivity.java
@@ -2,7 +2,7 @@ package com.pure.gothic.hackathon.idhackandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-// jae, lemonhall2, CYhan
+// jae, lemonhall2, CYhan, gon
 public class MainActivity extends AppCompatActivity {
 
     @Override
